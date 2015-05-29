b3b1e46dd73b313bac689367261b413828430b73
Konstantin Greger <kogreger@gmail.com>
2015-05-29 17:01:52 +0200

    tweaked code. included routines for loading census data (atributes and spatial). added function to change encoding fom SJIS to UTF-8

--- japan-nuclear-powerplants.R
+++ japan-nuclear-powerplants.R
@@ -1,16 +1,37 @@
-library(dplyr)
+# initialize
+## load libraries
 library(leafletR)
+library(dplyr)
 library(readr)
+library(maptools)
 library(sp)
 library(stringr)
 library(XML)
+
+## initialize fuctions
+### character encoding conversion SJIS -> UTF8 for data frames
+dfSjis2Utf8 <- function(data){
+    for (i in colnames(data)){
+        data[, i] <- iconv(data[, i], "CP932", "UTF-8")
+    }
+    return(data)
+}
+
+### character encoding conversion SJIS -> UTF8 for spatial data frames
+spSjis2Utf8 <- function(sp.data){
+    for (i in colnames(sp.data@data)){
+        sp.data@data[, i] <- iconv(sp.data@data[, i], "CP932", "UTF-8")
+    }
+    return(sp.data)
+}
+
 
 
 # load data
 
 ## power plant locations
 
-## scrape table from Wikipedia page
+### scrape table from Wikipedia page
 wikiUrl <- "http://en.wikipedia.org/wiki/Nuclear_power_in_Japan"
 tables <- readHTMLTable(wikiUrl)
 powerplants <- tables[[1]] %>% 
@@ -40,6 +61,47 @@ proj4string(powerplants) = CRS("+init=epsg:4326")
 powerplants$status <- gsub("[[[:digit:]]*]", "", powerplants$status)
 powerplants$status <- gsub("\n", " ", powerplants$status)
 
+
+## census data
+## data: http://www.e-stat.go.jp/SG1/estat/CsvdlE.do?sinfid=000012460662
+## type code table: http://www.stat.go.jp/data/kokusei/2010/users-g/pdf/03.pdf
+### load data, convert encoding to UTF8, and filter to relevant units
+census <- read_csv("./data/census.csv", 
+                   skip = 10, 
+                   col_types = "__cci_ci____d", 
+                   col_names = c("code", "type", "year", 
+                                 "name", "pop", "area")) %>% 
+    filter(type %in% c("0", "2", "3")) %>% 
+    as.data.frame() %>% 
+    dfSjis2Utf8()
+
+
+## census locations
+## data: http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03.html -> SHP
+### unzip data
+unzip("./data/shichouson2010.zip", exdir = "./data")
+### load data and convert encoding to UTF8
+shichouson <- readShapePoly("./data/N03-14_140401.shp", 
+                            proj4string=CRS("+init=epsg:4612")) %>% 
+    spSjis2Utf8()
+### remove temporary data
+file.remove(paste0(rep("./data/N03-14_140401.", 2), 
+                   c("dbf", "prj", "sbn", "sbx", "shp", "shx")))
+### extract, dissolve and filter attribute data
+shichousonData <- shichouson@data %>% 
+    select(code = N03_007, 
+           name = N03_004, 
+           pref = N03_001) %>% 
+    group_by(code) %>% 
+    summarise(name = min(name), 
+              pref = min(pref)) %>% 
+    filter(!is.na(code)) %>%
+    as.data.frame
+### dissolve multi-part polygons
+shichouson <- unionSpatialPolygons(shichouson, IDs = shichouson$N03_007)
+### combine polygon andf attribute data
+row.names(shichousonData) <- shichousonData$code
+shichouson <- SpatialPolygonsDataFrame(shichouson, shichousonData)
 
 
 # plot map using Leaflet
